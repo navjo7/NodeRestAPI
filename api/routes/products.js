315a8179244e795fd1becfe64f002385473453ce
navjo7 <navjotsinghdata@gmail.com>
2018-07-15 20:51:20 +1000

    curd mongoose

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
-
+const checkAuth = require('../middleware/check-auth')
 const Product = require('../models/product')
 
 router.get('/',(req,res,next)=>{
@@ -39,7 +39,7 @@ router.get('/',(req,res,next)=>{
     })
 })
 
-router.post('/',(req,res,next)=>{
+router.post('/', checkAuth, (req,res,next)=>{
    
     const product = new Product({
         _id : new mongoose.Types.ObjectId(),
@@ -67,8 +67,6 @@ router.post('/',(req,res,next)=>{
              error: e
          })
     })
-
-   
 })
 
 router.get('/:productId',(req,res,next)=>{
@@ -87,7 +85,7 @@ router.get('/:productId',(req,res,next)=>{
 
     })
 })
-router.patch('/:productId',(req,res,next)=>{
+router.patch('/:productId', checkAuth,(req,res,next)=>{
     const id = req.params.productId
     const updateOps = {}
 
@@ -101,7 +99,7 @@ router.patch('/:productId',(req,res,next)=>{
     })
 })
 
-router.delete('/:productId',(req,res,next)=>{
+router.delete('/:productId', checkAuth,(req,res,next)=>{
     const id = req.params.productId
     Product.findByIdAndRemove(id).then((result)=>{
         res.status(200).json(result)
--- api/routes/user.js
+++ api/routes/user.js
@@ -3,6 +3,7 @@ const router     = express.Router()
 const mongoose   = require('mongoose')
 const bcrypt     = require('bcrypt')
 const User       = require('../models/user')
+const jwt        = require("jsonwebtoken")
 const salt = 10
 router.post('/signup',(req,res,next)=>{
     bcrypt.hash(req.body.password,salt,(err,hash)=>{
@@ -29,6 +30,43 @@ router.post('/signup',(req,res,next)=>{
     })
 })
 
+router.post('/login',(req,res,next)=>{
+    User.find({email: req.body.email}).then((user)=>{
+        if(user.length<1){
+            return res.status(404).json({
+                message: "invalid email"
+            })
+        }
+
+        bcrypt.compare(req.body.password,user[0].password,(err,re)=>{
+            if(err){
+                return re.status(404).json({
+                    message: "Authentinvalid password"
+                })
+            }
+            if(re){
+                const token = jwt.sign({
+                        email:user[0].email,
+                        userId: user[0]._id,
+                        },
+                        process.env.JWT_KEY,
+                        {
+                            // expiresIn: "1"
+                        }
+                    )
+                return res.status(200).json({
+                    message: "Auth successfull",
+                    token : token
+                })
+            }
+            res.status(401).json({
+                message : "auth failed"
+            })
+        })
+    }).catch((e)=>{
+        res.status(500).json({error:e})
+    })
+})
 router.get('/',(req,res,next)=>{
     User.find().then((results)=>{
         res.status(200).json(results)
